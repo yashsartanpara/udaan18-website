913762ff7bc5c34c88a4ad83d3df3a09ea711217
sidx1024 <vistax86live@gmail.com>
2018-02-27 13:35:59 +0530

    Added yafps & other minor updates.

--- js/main.js
+++ js/main.js
@@ -3,43 +3,66 @@
 *=========================================================================================
 */
 
-$(document).ready(function () {
-  console.info('[main.js] : Ready')
-  setupCartridgeEvents()
-  setupInteractionEvents()
-})
+window.onload = function () {
+  console.info('[main.js] : Ready');
+  introComplete();
+  return; // This is intentional. (the intro animation is not complete).
+
+  var arcadeSVG = placeSvgIntoDocument('#arcadeSVG');
+  console.log(arcadeSVG);
+  var arcadeScreenPlaceholder = arcadeSVG.querySelector('#arcadeScreenPlaceholder');
+  window.arcadeScreenPlaceholder = arcadeScreenPlaceholder;
+  arcadeScreenPlaceholder.setAttribute('opacity', 0);
+  var arcadeScreen = document.querySelector('#arcadeScreen');
+  fitRectBounds(arcadeScreen, arcadeScreenPlaceholder);
+  var screenTextElements = arcadeScreen.querySelectorAll('.screenText');
+  [].forEach.call(screenTextElements, function (element) {
+    fitFontToContainer(element, 40);
+  });
+  var coin = document.querySelector('#coin');
+  console.log(coin);
+  var introTimeline = new TimelineMax({ repeat:-1, yoyo: true});
+  var coinZoomTimeline = new TimelineMax({ repeat:3, yoyo: true });
+  var coinFlashTimeline = new TimelineMax({ repeat:-1, yoyo: true });
+  coinZoomTimeline.from(coin, 1, { top: '-=5%', ease: Power0.easeNone, scale: 1.1});
+  coinFlashTimeline.from(coin, 1, { backgroundColor: 'rgba(255, 249, 15, 0.1)', ease: Power0.easeNone});
+  coin.addEventListener('click', function () {
+
+  });
+
+  console.log(arcadeScreen);
+};
 
 /*========================================================================================
 *   Setup events
 *=========================================================================================
 */
 
+function introComplete() {
+  setupCartridgeEvents();
+  setupInteractionEvents();
+  document.querySelector('#intro').style.display = 'none';
+}
+
 function setupInteractionEvents() {
   var pager = document.querySelector('.pager');
-  var pages = document.querySelectorAll('.page');
-  [].forEach.call(pages, function(page) {
-    page.addEventListener('touchstart', setActivePage);
-    page.addEventListener('click', setActivePage);
-    function setActivePage(event) {
-      window.activePage = event.currentTarget
-      console.log('activePage', window.activePage)
-    }
-  })
 
-  var yafpsPager = yafps(pager, {});
-  yafpsPager.setAnimationFunction(function (intent) {
-    TweenLite.to(intent.target, 0.5,
-      Object.assign(intent.toPercent, {onComplete: intent.callback}));
-  });
-  yafpsPager.move('right');
-  yafpsPager.setBeforeMoveFunction(function () {
-    clearCartridgeSelection(getActiveSection())
+  var yafpsPager = yafps(pager, {
+    hashChange: true,
+    startPage: '#udaan-title-page',
+    beforeMove: function () {
+      clearCartridgeSelection(getActiveSection());
+    },
+    animationFunction: function (intent) {
+      TweenLite.to(intent.target, 0.5,
+        Object.assign(intent.toPercent, {onComplete: intent.callback}));
+    }
   });
 
   var swipeListener = SwipeListener(pager, {lockAxis: true});
   pager.addEventListener('swipe', function (e) {
     var directions = e.detail.directions;
-    if(directions.left) {
+    if (directions.left) {
       yafpsPager.animate('right');
     } else if (directions.right) {
       yafpsPager.animate('left');
@@ -54,9 +77,9 @@ function setupInteractionEvents() {
 function setupCartridgeEvents() {
   var cartridges = document.querySelectorAll('.cartridge');
   [].forEach.call(cartridges, function (cartridge) {
-    cartridge.addEventListener('click', selectCartridge)
-  })
-  interactiveHit(slowDown)
+    cartridge.addEventListener('click', selectCartridge);
+  });
+  interactiveHit(slowDown);
 }
 
 /*========================================================================================
@@ -72,7 +95,7 @@ function setupCartridgeEvents() {
 var CARTRIDGE_NORMAL_Z_INDEX = 1,
   CARTRIDGE_SELECTED_Z_INDEX = 4,
   BLACKOUT_ON_Z_INDEX = 2,
-  BLACKOUT_OFF_Z_INDEX = 0
+  BLACKOUT_OFF_Z_INDEX = 0;
 
 /*==========================================
 *   ---- Cartridge Animation
@@ -81,20 +104,20 @@ var CARTRIDGE_NORMAL_Z_INDEX = 1,
 
 var CARTRIDGE_TRANSITION_IN_TIME = 1,
   CARTRIDGE_TRANSITION_OUT_TIME = 0.5,
-  EASE_ELASTIC = Elastic.easeOut.config(1.5, 0.5)
+  EASE_ELASTIC = Elastic.easeOut.config(1.5, 0.5);
 
 function selectCartridge(mouseEvent) {
-  var selectedCartridge = mouseEvent.currentTarget
+  var selectedCartridge = mouseEvent.currentTarget;
   if (selectedCartridge.classList.contains('cartridge--selected')) {
-    return
+    return;
   }
-  var centerPoint = getAbsoluteCenter(selectedCartridge)
-  var section = getActiveSection()
-  console.log('active-section', section)
+  var centerPoint = getAbsoluteCenter(selectedCartridge);
+  var section = getActiveSection();
+  console.log('active-section', section);
 
-  resetAllCartridges(selectedCartridge)
+  resetAllCartridges(selectedCartridge);
 
-  selectedCartridge.style.zIndex = CARTRIDGE_SELECTED_Z_INDEX
+  selectedCartridge.style.zIndex = CARTRIDGE_SELECTED_Z_INDEX;
 
   TweenLite.to(selectedCartridge, CARTRIDGE_TRANSITION_IN_TIME, {
     x: centerPoint.x,
@@ -102,49 +125,48 @@ function selectCartridge(mouseEvent) {
     scale: 1.2,
     ease: EASE_ELASTIC,
     onStart: changeConsoleState.bind(null, 'visible', getActiveSection())
-  })
+  });
   TweenLite.to(selectedCartridge, CARTRIDGE_TRANSITION_IN_TIME / 2, {
     onComplete: function () {
-      selectedCartridge.classList.add('cartridge--selected')
-      selectedCartridge.addEventListener('click', insertCartridge)
+      selectedCartridge.classList.add('cartridge--selected');
+      selectedCartridge.addEventListener('click', insertCartridge);
     }
-  })
-  blackout(section)
+  });
+  blackout(section);
 }
 
 function insertCartridge(mouseEvent) {
-  var cartridge = mouseEvent.currentTarget
+  var cartridge = mouseEvent.currentTarget;
   if (!cartridge.classList.contains('cartridge--selected'))
-    return
-  var section = getActiveSection()
-  var consoleTopView = section.querySelector('.console-top-view--top-half')
-  var cartridgeClippingPercent = 0.35
+    return;
+  var section = getActiveSection();
+  var consoleTopView = section.querySelector('.console-top-view--top-half');
+  var cartridgeClippingPercent = 0.35;
   if (getMedia().indexOf('xs portrait') > -1) {
-    cartridgeClippingPercent *= 1.45
+    cartridgeClippingPercent *= 1.45;
   }
-  var cartridgeClippingHeight = (cartridge.clientHeight) * (1 - cartridgeClippingPercent)
-  var point = consoleTopView.offsetTop - cartridge.offsetTop - cartridgeClippingHeight
+  var cartridgeClippingHeight = (cartridge.clientHeight) * (1 - cartridgeClippingPercent);
+  var point = consoleTopView.offsetTop - cartridge.offsetTop - cartridgeClippingHeight;
 
-  var url = ''
-  url = cartridge.getAttribute('data-href')
+  var url = cartridge.getAttribute('data-href');
 
   TweenLite.to(cartridge, 0.5, {
     y: point,
     onComplete: navigateTo.bind(null, url)
-  })
+  });
 }
 
 function resetAllCartridges(section, excluded) {
   var cartridges = section.querySelectorAll('.cartridge');
 
   [].forEach.call(cartridges, function (cartridge) {
-    cartridge.removeEventListener('click', insertCartridge)
-  })
+    cartridge.removeEventListener('click', insertCartridge);
+  });
 
   cartridges = [].filter.call(cartridges, function (cartridge) {
-    cartridge.classList.remove('cartridge--selected')
-    return cartridge !== excluded
-  })
+    cartridge.classList.remove('cartridge--selected');
+    return cartridge !== excluded;
+  });
 
   TweenLite.to(cartridges, CARTRIDGE_TRANSITION_IN_TIME, {
     x: 0,
@@ -154,40 +176,40 @@ function resetAllCartridges(section, excluded) {
   });
 
   [].forEach.call(cartridges, function (c) {
-    c.style.zIndex = CARTRIDGE_NORMAL_Z_INDEX
-  })
+    c.style.zIndex = CARTRIDGE_NORMAL_Z_INDEX;
+  });
 }
 
 function blackout(section) {
-  var blackout = section.querySelector('.blackout')
+  var blackout = section.querySelector('.blackout');
   if (!blackout)
-    return
-  blackout.style.zIndex = BLACKOUT_ON_Z_INDEX
+    return;
+  blackout.style.zIndex = BLACKOUT_ON_Z_INDEX;
 
   TweenLite.to(blackout, CARTRIDGE_TRANSITION_IN_TIME, {
     opacity: 0.4
-  })
+  });
 
   blackout.addEventListener('click', function onBlackout(mouseEvent) {
-    clearCartridgeSelection(section)
-    blackout.removeEventListener('click', onBlackout)
-  })
+    clearCartridgeSelection(section);
+    blackout.removeEventListener('click', onBlackout);
+  });
 }
 
 function clearCartridgeSelection(section) {
-  resetAllCartridges(section)
-  var blackout = section.querySelector('.blackout')
+  resetAllCartridges(section);
+  var blackout = section.querySelector('.blackout');
   if (!blackout)
-    return
-  blackout.style.zIndex = BLACKOUT_OFF_Z_INDEX
+    return;
+  blackout.style.zIndex = BLACKOUT_OFF_Z_INDEX;
   TweenLite.to(blackout, CARTRIDGE_TRANSITION_IN_TIME, {
     opacity: 0,
     onStart: changeConsoleState.bind(null, 'hidden', getActiveSection())
-  })
+  });
 }
 
 function getActiveSection() {
-  return window.activePage || document
+  return document.querySelector('.page--active') || document;
 }
 
 /*==========================================
@@ -196,11 +218,11 @@ function getActiveSection() {
 */
 
 var CONSOLE_TRANSITION_IN_TIME = 0.5,
-  CONSOLE_TRANSITION_OUT_TIME = CARTRIDGE_TRANSITION_OUT_TIME
+  CONSOLE_TRANSITION_OUT_TIME = CARTRIDGE_TRANSITION_OUT_TIME;
 
 function changeConsoleState(state, section) {
-  var consoleTopViewTopHalf = section.querySelector('.console-top-view--top-half')
-  var consoleTopViewBottomHalf = section.querySelector('.console-top-view--bottom-half')
+  var consoleTopViewTopHalf = section.querySelector('.console-top-view--top-half');
+  var consoleTopViewBottomHalf = section.querySelector('.console-top-view--bottom-half');
 
   switch (state) {
     case 'visible':
@@ -208,36 +230,36 @@ function changeConsoleState(state, section) {
         bottom: (consoleTopViewTopHalf.clientHeight),
         onStart: setConsoleState.bind(null, 'animating'),
         onComplete: setConsoleState.bind(null, 'visible')
-      })
+      });
       TweenLite.to(consoleTopViewBottomHalf, CONSOLE_TRANSITION_IN_TIME, {
         bottom: 0
-      })
-      break
+      });
+      break;
     case 'hidden':
       TweenLite.to(consoleTopViewTopHalf, CONSOLE_TRANSITION_OUT_TIME, {
         bottom: (consoleTopViewTopHalf.clientHeight * -1),
         onStart: setConsoleState.bind(null, 'animating'),
         onComplete: setConsoleState.bind(null, 'hidden')
-      })
+      });
       TweenLite.to(consoleTopViewBottomHalf, CONSOLE_TRANSITION_OUT_TIME, {
         bottom: (consoleTopViewBottomHalf.clientHeight * -1 * 2)
-      })
-      break
+      });
+      break;
   }
 }
 
 function getConsoleState() {
-  var section = getActiveSection()
+  var section = getActiveSection();
   var consoleTopViewTopHalf = section.querySelector(
-    '.console-top-view--top-half')
-  return consoleTopViewTopHalf.getAttribute('data-state')
+    '.console-top-view--top-half');
+  return consoleTopViewTopHalf.getAttribute('data-state');
 }
 
 function setConsoleState(state) {
-  var section = getActiveSection()
+  var section = getActiveSection();
   var consoleTopViewTopHalf = section.querySelector(
-    '.console-top-view--top-half')
-  return consoleTopViewTopHalf.setAttribute('data-state', state)
+    '.console-top-view--top-half');
+  return consoleTopViewTopHalf.setAttribute('data-state', state);
 }
 
 /*========================================================================================
@@ -253,66 +275,66 @@ function setConsoleState(state) {
 // TODO: Complete back button handle for mobile devices
 function backButton() {
   if (typeof history.pushState === 'function') {
-    history.pushState('jibberish', null, null)
+    history.pushState('jibberish', null, null);
     window.onpopstate = function () {
-      history.pushState('newjibberish', null, null)
+      history.pushState('newjibberish', null, null);
       // Handle the back (or forward) buttons here
       // Will NOT handle refresh, use onbeforeunload for this.
-    }
+    };
   }
   else {
-    var ignoreHashChange = true
+    var ignoreHashChange = true;
     window.onhashchange = function () {
       if (!ignoreHashChange) {
-        ignoreHashChange = true
-        window.location.hash = Math.random()
+        ignoreHashChange = true;
+        window.location.hash = Math.random();
         // Detect and redirect change here
         // Works in older FF and IE9
         // * it does mess with your hash symbol (anchor?) pound sign
         // delimiter on the end of the URL
       }
       else {
-        ignoreHashChange = false
+        ignoreHashChange = false;
       }
-    }
+    };
   }
 }
 
 function displayMessage(message) {
-  var userMessageBar = document.querySelector('#user-message-bar')
-  userMessageBar.innerHTML = message
-  userMessageBar.style.left = getActiveSection().style.left
+  var userMessageBar = document.querySelector('#user-message-bar');
+  userMessageBar.innerHTML = message;
+  userMessageBar.style.left = getActiveSection().style.left;
   TweenLite.to(userMessageBar, 1, {
     opacity: 1,
     ease: EASE_ELASTIC,
     onComplete: function () {
-      TweenLite.to(userMessageBar, 0.3, {opacity: 0})
+      TweenLite.to(userMessageBar, 0.3, {opacity: 0});
     }
-  })
+  });
 }
 
 function slowDown() {
-  displayMessage(getRandomInt() & 1 ? 'Hold your horses!' : 'Slow Down!')
+  displayMessage(getRandomInt() & 1 ? 'Hold your horses!' : 'Slow Down!');
 }
 
 function interactiveHit(callback) {
   if (!window._interativeHit) {
-    window._interactiveHit = 0
+    window._interactiveHit = 0;
   }
-  window.addEventListener('click', hitEvent)
-  window.addEventListener('touchstart', hitEvent)
+  window.addEventListener('click', hitEvent);
+  window.addEventListener('touchstart', hitEvent);
 
   function hitEvent() {
     if (window._interactiveHit > 4) {
-      callback()
+      callback();
     }
-    window._interactiveHit++
+    window._interactiveHit++;
   }
 
-  setInterval(resetHits, 1000)
+  setInterval(resetHits, 1000);
 
   function resetHits() {
-    window._interactiveHit = 0
+    window._interactiveHit = 0;
   }
 }
 
@@ -320,16 +342,44 @@ function interactiveHit(callback) {
 *   ---- Miscellaneous
 *===========================================
 */
+function fitFontToContainer(container, percent) {
+  var containerHeight = container.clientHeight * (percent / 100);
+  container.style.fontSize = containerHeight + 'px';
+}
+
+function placeSvgIntoDocument(selector) {
+  var object = document.querySelector(selector);
+  if (!object) {
+    console.error('Couldn\'t find ', selector);
+    return;
+  }
+  var svg = object.contentDocument.firstElementChild;
+  object.parentElement.replaceChild(svg, object);
+  svg.setAttribute('id', selector);
+  return svg;
+}
+
+function fitRectBounds(object, referenceObject) {
+  if (!object || !referenceObject) {
+    console.error('Error transferring attributes. Either object is empty.');
+  }
+  var boundingRect = referenceObject.getBoundingClientRect();
+  object.style.width = boundingRect.width + 'px';
+  object.style.height = boundingRect.height + 'px';
+  object.style.left = boundingRect.left + 'px';
+  object.style.top = boundingRect.top + 'px';
+}
+
 function getMedia() {
-  return getComputedStyle(document.querySelector('.media-query')).content
+  return getComputedStyle(document.querySelector('.media-query')).content;
 }
 
 function navigateTo(url) {
   if (url.length < 3) {
-    displayMessage('GAME UNDER DEVELOPMENT!')
-    return
+    displayMessage('GAME UNDER DEVELOPMENT!');
+    return;
   }
-  window.location.href = url
+  window.location.href = url;
 }
 
 /*==========================================
@@ -338,7 +388,7 @@ function navigateTo(url) {
 */
 
 function getRandomInt() {
-  return Math.round(Math.random() * 100)
+  return Math.round(Math.random() * 100);
 }
 
 /*==========================================
@@ -350,7 +400,7 @@ function getWindowCenter() {
   return {
     x: window.innerWidth / 2,
     y: window.innerHeight / 2
-  }
+  };
 }
 
 function getCenter(object, relativeTo) {
@@ -358,20 +408,20 @@ function getCenter(object, relativeTo) {
     return {
       x: object.offsetLeft + object.clientWidth / 2,
       y: object.offsetTop + object.clientHeight / 2
-    }
+    };
   }
   return { // relativeTo : (0, 0)
     x: object.clientWidth / 2,
     y: object.clientHeight / 2
-  }
+  };
 }
 
 function getAbsoluteCenter(object) {
-  var screenCenter = getWindowCenter()
-  var objectCenter = getCenter(object, 'document')
+  var screenCenter = getWindowCenter();
+  var objectCenter = getCenter(object, 'document');
 
-  var x = screenCenter.x - objectCenter.x
-  var y = screenCenter.y - objectCenter.y
+  var x = screenCenter.x - objectCenter.x;
+  var y = screenCenter.y - objectCenter.y;
 
-  return {x: x, y: y}
+  return {x: x, y: y};
 }
