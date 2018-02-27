8406118001c0c6dda48d5364b8ea396d3951cf2f
Yash Sartanpara <yashsartanpara@gmail.com>
2018-02-27 16:19:07 +0530

    Minor fixes (#25)
    * Code optimization
    
    * Code optimization

--- js/sf.js
+++ js/sf.js
@@ -1,24 +1,27 @@
 $('.guile').append('<img src="./img/Guile-oldstance.gif" height="192" width="106"/> ')
 $('.ryu').append('<img src="./img/ryu_bounce.gif" height="192"/>');
 
-$('html').mousedown(function() {
+$('html').mousedown(function () {
   $('.ryu img').attr('src', './img/ryuhad.png');
 })
-$('html').mouseup(function() {
+
+$('html').mouseup(function () {
   $('.kosign').remove();
-  $('.ryu img').attr('src','./img/ryu_bounce.gif');
+  $('.ryu img').attr('src', './img/ryu_bounce.gif');
 })
-$('html').mousedown(function() {
+
+$('html').mousedown(function () {
   $('.hudo').append(
     '<img class="hadouken" src="./img/hadouken.gif">'
   );
 })
-$('html').mousedown(function() {
+
+$('html').mousedown(function () {
   $('.hadouken').animate({
     "margin-left": "45px"
-  }, 0900, 'swing', function() {
+  }, 0900, 'swing', function () {
     this.remove();
     $('.kosign').append('<img src="http://www.slateman.net/images/gaming/gifs/mvc-ko.gif" />');
   })
-  })
+})
 
