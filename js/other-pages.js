46a64e632393db123ccb40a7e5b03e518e9de288
Vatsal Trivedi <trivedivatsal005@gmail.com>
2018-03-10 18:20:09 +0530

    Add Udaan Nights page Remove unused gsap files Add Developers page

--- js/other-pages.js
+++ js/other-pages.js
@@ -5,6 +5,7 @@
 var currentPath = location.pathname.slice(1, location.pathname.lastIndexOf('.'));
 
 window.onload = function () {
+  console.log(currentPath);
   switch (currentPath) {
     case 'udaan-nights':
       console.info('[INFO] Udaan Nights Page');
@@ -22,49 +23,86 @@ function setupUdaanNightsPage() {
 }
 
 function setupDevelopersPage() {
-  var windowsKeywords = [
-    'XAML', 'C#', 'FLUENT DESIGN', 'MVVM', ''
-  ];
-  var webKeywords = [
-    'GSAP', 'JS', 'FLEX', 'CONSOLE', 'EVENT LISTENER', 'VUE', 'REACT', 'DOM', 'WEBPACK'
-  ];
-  var androidKeywords = [
-    'MATERIAL DESIGN', 'KOTLIN', 'FIREBASE'
-  ];
-  var backendKeywords = [
-    'NODE', 'REST API', 'MONGO-DB', 'NPM', 'HEROKU', 'GIT', 'SWAGGER', 'EXPRESS'
-  ];
-  var dataAnalyticsKeywords = [
-    'PYTHON', 'PANDAS', 'MAP', 'REDUCE', 'FILTER', 'LAMBDA'
-  ];
-  var graphicsKeywords = [
-    'COREL', 'STROKE', 'COLOR', 'FILL', 'SVG', 'PNG', 'CMYK', 'RGB'
-  ];
-  var iosKeywords = [
-    'SWIFT'
-  ];
-
-  var constants = {
-    MAX_WORDS_PER_UNIT_TIME: 5
+  var keywords = {
+    windows: ['XAML', 'C#', 'FLUENT DESIGN', 'MVVM'],
+    android: ['MATERIAL DESIGN', 'KOTLIN', 'FIREBASE'],
+    backend: ['NODE', 'REST API', 'MONGO-DB', 'NPM', 'HEROKU', 'GIT', 'SWAGGER', 'EXPRESS'],
+    graphics: ['COREL', 'STROKE', 'COLOR', 'FILL', 'SVG', 'PNG', 'CMYK', 'RGB'],
+    dataAnalytics: ['PYTHON', 'PANDAS', 'MAP', 'REDUCE', 'FILTER', 'LAMBDA'],
+    ios: ['SWIFT'],
+    web: ['GSAP', 'JS', 'FLEX', 'CONSOLE', 'EVENT LISTENER', 'VUE', 'REACT', 'DOM', 'WEBPACK']
   };
 
-  var activeSectionKeywords = windowsKeywords;
-  var wordCache = [];
+  var constants = {
+    MAX_WORDS_PER_UNIT_TIME: 5,
+    POSITIONS: [{"x": 0.0234375, "y": 0.11458333333333333}, {"x": 0.075, "y": 0.14583333333333334}, {
+      "x": 0.04296875,
+      "y": 0.4270833333333333
+    }, {"x": 0.1421875, "y": 0.6458333333333334}, {"x": 0.24140625, "y": 0.16319444444444445}, {
+      "x": 0.32421875,
+      "y": 0.78125
+    }, {"x": 0.39765625, "y": 0.25}, {"x": 0.50234375, "y": 0.11458333333333333}, {
+      "x": 0.56328125,
+      "y": 0.6979166666666666
+    }, {"x": 0.63828125, "y": 0.25}, {"x": 0.68359375, "y": 0.5520833333333334}, {
+      "x": 0.76328125,
+      "y": 0.20833333333333334
+    }, {"x": 0.8265625, "y": 0.6180555555555556}, {"x": 0.8578125, "y": 0.2743055555555556}, {
+      "x": 0.88828125,
+      "y": 0.4166666666666667
+    }, {"x": 0.79765625, "y": 0.4722222222222222}, {"x": 0.7171875, "y": 0.2638888888888889}, {
+      "x": 0.678125,
+      "y": 0.4097222222222222
+    }]
+  };
 
-  for(var i = 0; i < constants.MAX_WORDS_PER_UNIT_TIME; i++) {
-    wordCache.push(activeSectionKeywords[i]);
+  var activeSectionKeywords = keywords.web;
+  var elements = generateElements(activeSectionKeywords);
+  setPositions(elements);
+
+  var header = document.querySelector('#developers .header');
+  var headerWidth = header.clientWidth;
+  var headerHeight = header.clientHeight;
+  var seed = getRandomInt(0, constants.POSITIONS.length - 1);
+  var tl = new TimelineMax({repeat: -1});
+  tl.add(TweenMax.staggerFromTo(elements, 0.5, {
+    opacity: 0, top: "+=16px"
+  }, {
+    opacity: 1, top: "-=16px"
+  }, 0));
+  tl.add(TweenMax.staggerTo(elements, 0.5, {
+    opacity: 0, top: "-=16px"
+  }, 0.5), 0);
+
+  function setPositions(elements) {
+    var canvas = document.querySelector('#developers .header');
+    var width = canvas.clientWidth;
+    var height = canvas.clientHeight;
+    var usedIndices = [];
+    elements.map(function (element) {
+      canvas.appendChild(element);
+      var elementWidth = element.clientWidth;
+      var elementHeight = element.clientHeight;
+      var randomIndex = getRandomInt(0, constants.POSITIONS.length - 1);
+      while (usedIndices.indexOf(randomIndex) !== -1) {
+        randomIndex = getRandomInt(0, constants.POSITIONS.length - 1);
+      }
+      usedIndices.push(randomIndex);
+      var position = constants.POSITIONS[randomIndex];
+      element.style.left = (position.x * width) + 'px';
+      element.style.top = (position.y * height) + 'px';
+    });
   }
-
-  var wordCloudTimeline = new TimelineMax({});
-
 
   function generateElements(keywords) {
     var elements = [];
     keywords.map(function (keyword) {
-      var keywordElement = document.createElement('.keyword');
+      var keywordElement = document.createElement('div');
+      keywordElement.classList.add('keyword');
       keywordElement.innerText = keyword;
-      elements.push(keyword);
+      elements.push(keywordElement);
     });
+    return elements;
   }
 
   function getRandomInt(min, max) {
@@ -72,5 +110,4 @@ function setupDevelopersPage() {
     max = max || 100;
     return min + Math.round(Math.random() * max);
   }
-
 }
