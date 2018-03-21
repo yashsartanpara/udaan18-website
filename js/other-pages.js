a343932d618fbf89a6d43154ba46d3768ce5dcda
sidx1024 <vistax86live@gmail.com>
2018-03-21 15:24:59 +0530

    Change data source to json

--- js/other-pages.js
+++ js/other-pages.js
@@ -35,23 +35,31 @@ function setupUdaanDevelopersPage() {
 
   var activeSectionKeywords = keywords.web;
   var developersBlockContainer = document.querySelector('#developers-block-container');
-  var data = transform(developersData());
-  for (var i = 0; i < data.length; i++) {
-    var category = data[i].category;
-    var members = data[i].members;
-    developersBlockContainer.appendChild(newSeparator(category.toUpperCase()));
-    for (var j = 0; j < members.length; j++) {
-      var member = members[j];
-      var memberElement = newMember({
-        name: member.name.toUpperCase(),
-        title: member.title.toUpperCase()
-      });
-      memberElement.setAttribute('data-href', member.github);
-      memberElement.addEventListener('click', function (mouseEvent) {
-        var target = mouseEvent.currentTarget;
-        window.open(target.getAttribute('data-href'));
-      });
-      developersBlockContainer.appendChild(memberElement);
+
+  fetch('./developers.json')
+    .then(function(response) { return response.json() })
+    .then(transform)
+    .then(onDataReceived)
+    .catch(console.error);
+
+  function onDataReceived(data) {
+    for (var i = 0; i < data.length; i++) {
+      var category = data[i].category;
+      var members = data[i].members;
+      developersBlockContainer.appendChild(newSeparator(category.toUpperCase()));
+      for (var j = 0; j < members.length; j++) {
+        var member = members[j];
+        var memberElement = newMember({
+          name: member.name.toUpperCase(),
+          title: member.title.toUpperCase()
+        });
+        memberElement.setAttribute('data-href', member.github);
+        memberElement.addEventListener('click', function (mouseEvent) {
+          var target = mouseEvent.currentTarget;
+          window.open(target.getAttribute('data-href'));
+        });
+        developersBlockContainer.appendChild(memberElement);
+      }
     }
   }
 
@@ -129,158 +137,33 @@ function setupUdaanDevelopersPage() {
     });
     return Object.values(categoryList);
   }
-
-  function developersData() {
-    return [
-      {
-        "title": "Web Developer",
-        "color": "",
-        "category": "web",
-        "name": "Vatsal Trivedi",
-        "mobile": "+917600635053",
-        "email": "trivedivatsal005@gmail.com",
-        "github": "https://github.com/vattytrivedi"
-      },
-      {
-        "title": "Web Developer",
-        "color": "",
-        "category": "web",
-        "name": "Yash Sartanpara",
-        "mobile": "+919724052113",
-        "email": "yash@captaintorch.tech",
-        "github": "https://captaintorch.tech"
-      },
-      {
-        "title": "Web Developer",
-        "color": "",
-        "category": "web",
-        "name": "Siddharth Goswami",
-        "mobile": "+919723461024",
-        "email": "_@siddharth.xyz",
-        "github": "https://github.com/sidx1024"
-      },
-      {
-        "title": "Back-End Developer",
-        "color": "",
-        "category": "backend",
-        "name": "Chintan Acharya",
-        "mobile": "+918140474055",
-        "email": "acharya.chintan96@gmail.com",
-        "github": "https://github.com/chintanacharya"
-      },
-      {
-        "title": "UWP Developer",
-        "color": "",
-        "category": "windows",
-        "name": "Darshan Dalal",
-        "mobile": "+918460157727",
-        "email": "darshandalal3@outlook.com",
-        "github": "https://github.com/darshan010"
-      },
-      {
-        "title": "Android Developer",
-        "color": "",
-        "category": "unity",
-        "name": "Jay Patel",
-        "mobile": "+919374883988",
-        "email": "jp9573@gmail.com",
-        "github": "https://github.com/jp9573"
-      },
-      {
-        "title": "Web Developer",
-        "color": "",
-        "category": "web",
-        "name": "Megharth Lakhataria",
-        "mobile": "+919429000091",
-        "email": "m.c.lakhataria@gmail.com",
-        "github": "https://github.com/megharth"
-      },
-      {
-        "title": "Python Developer",
-        "color": "",
-        "category": "ai",
-        "name": "Pranshu Dave",
-        "mobile": "+918238330100",
-        "email": "pranshudave@gmail.com",
-        "github": "https://github.com/pranshu0210"
-      },
-      {
-        "title": "Back-End Developer",
-        "color": "",
-        "category": "backend",
-        "name": "Rushi Rami",
-        "mobile": "+917069307537",
-        "email": "",
-        "github": "https://github.com/rushi7997"
-      },
-      {
-        "title": "Back-End Developer",
-        "color": "",
-        "category": "backend",
-        "name": "Jay Movaliya",
-        "mobile": "+918160762644",
-        "email": "jaymovaliya786@gmail.com",
-        "github": "https://github.com/jaymovaliya"
-      },
-      {
-        "title": "Android Developer",
-        "color": "",
-        "category": "android",
-        "name": "Bhagyesh Radiya",
-        "mobile": "+918141435149",
-        "email": "",
-        "github": "https://github.com/BhagyeshRadiya2796"
-      },
-      {
-        "title": "Android Developer",
-        "color": "",
-        "category": "android",
-        "name": "Abhi Akbari",
-        "mobile": "+919909214688",
-        "email": "abhiakbari023@gmail.com",
-        "github": "https://github.com/abhi055"
-      },
-      {
-        "title": "iOS Developer",
-        "color": "",
-        "category": "ios",
-        "name": "Jay Vaghani",
-        "mobile": "+919909834387",
-        "email": "jvaghani971@gmail.com",
-        "github": "https://github.com/jayvaghani"
-      },
-      {
-        "title": "Android Developer",
-        "color": "",
-        "category": "android",
-        "name": "Pranav Gajera",
-        "mobile": "+91",
-        "email": "",
-        "github": "https://github.com/prnv28"
-      }
-    ]
-  }
 }
 
 function setupUdaanTeamPage() {
   var teamBlockContainer = document.querySelector('.team-block-container');
 
-  var data = teamData();
-  for (var i = 0; i < data.length; i++) {
-    var category = data[i].category;
-    var members = data[i].members;
-    for (var j = 0; j < members.length; j++) {
-      var member = members[j];
-      var memberElement = newMember({
-        name: member.name.toUpperCase(),
-        title: category + ' ' + member.title,
-        large: i < 2,
-        image: ''
-      });
-      teamBlockContainer.appendChild(memberElement);
-      if (i === 1) {
-        var separatorElement = newSeparator();
-        teamBlockContainer.appendChild(separatorElement);
+  fetch('./teamudaan.json')
+    .then(function(response) { return response.json() })
+    .then(onDataReceived)
+    .catch(console.error);
+
+  function onDataReceived(data) {
+    for (var i = 0; i < data.length; i++) {
+      var category = data[i].category;
+      var members = data[i].members;
+      for (var j = 0; j < members.length; j++) {
+        var member = members[j];
+        var memberElement = newMember({
+          name: member.name.toUpperCase(),
+          title: category + ' ' + member.title,
+          large: i < 2,
+          image: ''
+        });
+        teamBlockContainer.appendChild(memberElement);
+        if (i === 1) {
+          var separatorElement = newSeparator();
+          teamBlockContainer.appendChild(separatorElement);
+        }
       }
     }
   }
@@ -333,173 +216,4 @@ function setupUdaanTeamPage() {
     return separatorElement;
   }
 
-  function teamData() {
-    return [
-      {
-        'category': 'General Secretary',
-        'members': [
-          {
-            'name': 'Jatan Patel',
-            'title': ''
-          }
-        ]
-      },
-      {
-        'category': 'Ladies Representative',
-        'members': [
-          {
-            'name': 'Megha Patel',
-            'title': ''
-          }
-        ]
-      },
-      {
-        'category': 'Tech',
-        'members': [
-          {
-            'name': 'Harshil Jani',
-            'title': 'Head'
-          },
-          {
-            'name': 'Umang Patel',
-            'title': 'Head'
-          },
-          {
-            'name': 'Pradeep Dodiya',
-            'title': 'Head'
-          },
-          {
-            'name': 'Megh Shah',
-            'title': 'Head'
-          }
-        ]
-      },
-      {
-        'category': 'Publicity',
-        'members': [
-          {
-            'name': 'Anshul Thacker',
-            'title': 'Head'
-          },
-          {
-            'name': 'Rejoice Paracal',
-            'title': 'Head'
-          }
-        ]
-      },
-      {
-        'category': 'Documentation',
-        'members': [
-          {
-            'name': 'Shreyansh Aghera',
-            'title': 'Head'
-          },
-          {
-            'name': 'Udit Patel',
-            'title': 'Head'
-          }
-        ]
-      },
-      {
-        'category': 'Logistics',
-        'members': [
-          {
-            'name': 'Harsh Vora',
-            'title': 'Head'
-          },
-          {
-            'name': 'Hiren Thakkar',
-            'title': 'Head'
-          }
-        ]
-      },
-      {
-        'category': 'Decoration',
-        'members': [
-          {
-            'name': 'Hardik Shilu',
-            'title': 'Head'
-          }
-        ]
-      },
-      {
-        'category': 'Developers',
-        'members': [
-          {
-            'name': 'Vatsal Trivedi',
-            'title': 'Head'
-          },
-          {
-            'name': 'Chintan Acharya',
-            'title': 'Head'
-          }
-        ]
-      },
-      {
-        'category': 'Graphics',
-        'members': [
-          {
-            'name': 'Tushar Gonavala',
-            'title': 'Head'
-          }
-        ]
-      },
-      {
-        'category': 'Discipline',
-        'members': [
-          {
-            'name': 'King Tandel',
-            'title': 'Head'
-          },
-          {
-            'name': 'Manan Shah',
-            'title': 'Head'
-          },
-          {
-            'name': 'Dharam Patel',
-            'title': 'Head'
-          }
-        ]
-      },
-      {
-        'category': 'Photography',
-        'members': [
-          {
-            'name': 'Punit Suthar',
-            'title': 'Head'
-          },
-          {
-            'name': 'Vivek Bariya',
-            'title': 'Head'
-          }
-        ]
-      },
-      {
-        'category': 'Cultural',
-        'members': [
-          {
-            'name': 'Kishan Patel',
-            'title': 'Head'
-          },
-          {
-            'name': 'Meet Pachchigar',
-            'title': 'Head'
-          }
-        ]
-      },
-      {
-        'category': 'Sponsorship',
-        'members': [
-          {
-            'name': 'Deep Lakkad',
-            'title': 'Head'
-          },
-          {
-            'name': 'Vatsal Rami',
-            'title': 'Head'
-          }
-        ]
-      }
-    ];
-  }
 }
